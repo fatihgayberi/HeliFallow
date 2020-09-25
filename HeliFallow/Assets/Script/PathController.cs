106bd582dedda3518be8b21c0594eb0221a87a61
Fatih Gayberi <fatihgayberii@gmail.com>
2020-09-25 15:08:09 +0300

    Thief
    Thief chasing. Velocity Problem

--- HeliFallow/Assets/Script/PathController.cs
+++ HeliFallow/Assets/Script/PathController.cs
@@ -4,43 +4,16 @@ using UnityEngine;
 
 public class PathController : MonoBehaviour
 {
-    [SerializeField] GameObject[] pathArray;
-    [SerializeField] float pithSpeed = 3f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PathTrans();
-    }
+    [SerializeField] GameObject thief; // oyundaki tum pathler
 
     // path baslangic pozisyonunu tutar.
-    Vector3 pathStartPosition { get { return new Vector3(40, 0, 0); } }
+    Vector3 pathStartPosition { get { return new Vector3(Mathf.Floor(thief.transform.position.x) + 50f, 0f, 0f); } }
 
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position = pathStartPosition;
-        other.GetComponent<Rigidbody>().velocity = Vector3.zero;
-    }
-
-    // pathin X ekseninde surekli kaymasini saglar.
-    void PathPositionUpdate(GameObject path)
-    {
-        path.GetComponent<Rigidbody>().velocity = new Vector3(-pithSpeed, 0, 0);
-    }
-
-    // tum pathlere kaydirma islemi uygular.
-    void PathTrans()
-    {
-        for (int i = 0; i < pathArray.Length; i++)
+        if (other.tag == "Path")
         {
-            PathPositionUpdate(pathArray[i]);
+            other.transform.position = pathStartPosition;
         }
     }
-
 }
---
+++ HeliFallow/Assets/Script/ThiefMoved.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefMoved : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    Rigidbody rbThief;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbThief = GetComponent<Rigidbody>();
+    }
+
+    // thiefin hareket etmesini saglar
+    void ThiefMove()
+    {
+        rbThief.velocity = new Vector3(speed, rbThief.velocity.y, rbThief.velocity.z);
+    }
+
+    private void FixedUpdate()
+    {
+        ThiefMove();
+    }
+}
